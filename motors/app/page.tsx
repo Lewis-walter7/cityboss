aa082cf1742e375757ec699051dc2ba7ded624c6
Lewis Indusa <lewiswalter192@gmail.com>
2025-12-12 12:35:55 +0300

    Err Clean up

--- motors/app/page.tsx
+++ motors/app/page.tsx
@@ -2,7 +2,7 @@ import { Navbar } from '@/components/layout/Navbar';
 import { Footer } from '@/components/layout/Footer';
 import { NewHero } from '@/components/home/NewHero';
 import { CategoryGrid } from '@/components/home/CategoryGrid';
-import { FeaturedListings } from '@/components/home/FeaturedListings';
+import FeaturedListings from '@/components/home/FeaturedListings';
 import { Testimonials } from '@/components/home/Testimonials';
 
 import { FAQ } from '@/components/home/FAQ';
